457d51c05e9138de8da67efa17b4df66828d9b46
DanielPerlman <dandandaman02@gmail.com>
2018-11-29 10:02:10 +0100

    Added code for removing and updating todos status.

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -14,3 +14,20 @@ exports.todo_create = function (req, res) {
         res.send('Todo added successfully')
     })
 };
+
+// Find specified document and set removed to current date and time.
+exports.todo_remove = function (req, res) {
+    Todo.findByIdAndUpdate(req.params.id, {$set: {removedAt: Date.now()}}, function (err, todo) {
+        if (err) return next(err);
+        res.send('Todo removed.');
+    });
+};
+
+// Find specified document and mark as done or not based on param
+exports.todo_update_status = function (req, res) {
+    Todo.findOneAndUpdate(req.params.id, {$set: {done: req.body.done}}, function (err, todo) {
+        if (err) return next(err);
+        let markedText = req.body.done == 'true' ? 'marked' : 'unmarked';
+        res.send(`Todo ${markedText} as done.`);
+    });
+};
--- routes.js
+++ routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const todo_controller = require('./controllers/todo.controller');
 
 router.post('/create', todo_controller.todo_create);
+router.delete('/:id/delete', todo_controller.todo_remove);
+router.put('/:id/update_status', todo_controller.todo_update_status);
 
 module.exports = router;
